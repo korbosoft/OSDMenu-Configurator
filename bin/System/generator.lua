836433aff49b8528e684c4ac16589b3cc8af2c93
Korbo <korbin.m.marshall@gmail.com>
2025-11-25 20:42:11 -0600

    init

---
+++ bin/System/generator.lua
@@ -0,0 +1,43 @@
+OSDSYS_video_mode = AUTO\z
+OSDSYS_Inner_Browser = 0\z
+OSDSYS_selected_color = 0x10,0x80,0xE0,0x80\z
+OSDSYS_unselected_color = 0x33,0x33,0x33,0x80\z
+OSDSYS_scroll_menu = 1\z
+OSDSYS_menu_x = 320\z
+OSDSYS_menu_y = 110\z
+OSDSYS_enter_x = 30\z
+OSDSYS_enter_y = -1\z
+OSDSYS_version_x = -1\z
+OSDSYS_version_y = -1\z
+OSDSYS_cursor_max_velocity = 1500\z
+OSDSYS_cursor_acceleration = 150\z
+OSDSYS_left_cursor =\z
+OSDSYS_right_cursor =\z
+OSDSYS_menu_top_delimiter =\z
+OSDSYS_menu_bottom_delimiter =\z
+OSDSYS_num_displayed_items = 5\z
+OSDSYS_Skip_MC = 1\z
+OSDSYS_Skip_HDD = 1\z
+OSDSYS_Skip_Disc = 1\z
+OSDSYS_Skip_Logo = 1\z
+cdrom_skip_ps2logo = 1\z
+cdrom_disable_gameid = 0\z
+cdrom_use_dkwdrv = 0\z
+ps1drv_enable_fast = 0\z
+ps1drv_enable_smooth = 0\z
+ps1drv_use_ps1vn = 0\z
+app_gameid = 0\z
+# path_DKWDRV_ELF = mc?:/BOOT/DKWDRV.ELF\z
+# --------------------------------------------------\z
+name_OSDSYS_ITEM_1 = Launch Disc\z
+path1_OSDSYS_ITEM_1 = cdrom\z
+arg_OSDSYS_ITEM_1 = -nologo\z
+# --------------------------------------------------\z
+name_OSDSYS_ITEM_2 = wLaunchELF\z
+path1_OSDSYS_ITEM_2 = mc?:/BOOT/BOOT.ELF\z
+path2_OSDSYS_ITEM_2 = mmce?:/apps/wLaunchELF.elf\z
+path3_OSDSYS_ITEM_2 = usb:/apps/wLaunchELF.elf\z
+# --------------------------------------------------\z
+name_OSDSYS_ITEM_250 = Shutdown\z
+path1_OSDSYS_ITEM_250 = POWEROFF\z
+# --------------------------------------------------\z
