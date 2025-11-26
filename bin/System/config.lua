c29f49ee63099fbdffd9f011ba7ffd948a593f40
Korbo <korbin.m.marshall@gmail.com>
2025-11-26 13:48:20 -0600

    big changes

---
+++ bin/System/config.lua
@@ -0,0 +1,27 @@
+OSDSYS_video_mode = "AUTO"
+OSDSYS_Inner_Browser = 0
+OSDSYS_selected_color = Color.new(16, 128, 224, 128)
+OSDSYS_unselected_color = Color.new(51, 51, 51, 128)
+OSDSYS_scroll_menu = 1
+OSDSYS_menu_x = 320
+OSDSYS_menu_y = 110
+OSDSYS_enter_x = 30
+OSDSYS_enter_y = -1
+OSDSYS_version_x = -1
+OSDSYS_version_y = -1
+OSDSYS_cursor_max_velocity = 1500
+OSDSYS_cursor_acceleration = 150
+OSDSYS_left_cursor = ""
+OSDSYS_right_cursor = ""
+OSDSYS_menu_top_delimiter = ""
+OSDSYS_menu_bottom_delimiter = ""
+OSDSYS_num_displayed_items = 5
+OSDSYS_Skip_MC = 1
+OSDSYS_Skip_HDD = 1
+OSDSYS_Skip_Disc = 1
+OSDSYS_Skip_Logo = 1
+
+cdrom_skip_ps2logo = 1
+cdrom_disable_gameid = 0
+cdrom_use_dkwdrv = 0
+app_gameid = 0
